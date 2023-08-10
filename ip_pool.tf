6f9dbbabf249abd5b4aaa2fed63a31a3fbe369be
Chris Marget <cmarget@juniper.net>
2023-08-10 15:25:34 -0400

    add 2nd network

--- ip_pool.tf
+++ ip_pool.tf
@@ -1,9 +1,7 @@
 resource "apstra_ipv4_pool" "test" {
   name = "gitops"
   subnets = [
-    {
-      network = "192.168.1.0/24"
-      network = "192.168.2.0/24"
-    }
+    { network = "192.168.1.0/24" },
+    { network = "192.168.2.0/24" },
   ]
 }
